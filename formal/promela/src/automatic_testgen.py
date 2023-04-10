ae0f303a27e2bad241d6d9bd052b8dc6dc2c622e
GH-James <78970503+GH-James@users.noreply.github.com>
2023-04-10 22:54:15 +0100

    Added more functionality to automatic_testgen.py to match that of testbuilder.py #16 automatic_testgen_template.yml - generate promela files - generate spin files - generate test files - clean - compile - run

--- formal/promela/src/automatic_testgen.py
+++ formal/promela/src/automatic_testgen.py
@@ -1,11 +1,12 @@
 from src.modules.promela_yacc.promela import yacc, ast, lex
+import glob
+import os
 import sys
-import pathlib
-import filecmp
 import yaml
 from dataclasses import dataclass
-from typing import Union, List, Any
+from typing import Union, List
 from pathlib import Path
+import testbuilder
 
 
 @dataclass
@@ -154,54 +155,138 @@ def get_config(source_dir):
     config = dict()
     required_keys = {"disable_negation_at", "max_trails", "spin_assert",
                      "spin_ltl"}
-    with open(f"{source_dir}/automatic_tesgen.yml") as file:
+    with open(f"{source_dir}/automatic_testgen.yml") as file:
         global_config = yaml.load(file, Loader=yaml.FullLoader)
         for key, val in global_config.items():
             config[key] = val
-    if Path("automatic_tesgen.yml").exists():
-        with open("automatic_tesgen.yml") as file:
+    if Path("automatic_testgen.yml").exists():
+        with open("automatic_testgen.yml") as file:
             local_config = yaml.load(file, Loader=yaml.FullLoader)
             if local_config:
                 for key, val in local_config.items():
                     config[key] = val
     missing_keys = required_keys - config.keys()
     if missing_keys:
-        print("automatic_tesgen.yml configuration is incomplete")
+        print("automatic_testgen.yml configuration is incomplete")
         print("The following configuration items are missing:")
         for key in missing_keys:
             print(key)
         sys.exit(1)
+    return config
 
 
-def get_programs(model_file):
+def get_generated_pml_files() -> List[str]:
+    generated_pml_files = glob.glob("assert*.pml")
+    generated_pml_files += glob.glob("ltl*.pml")
+    return generated_pml_files
+
+
+def get_generated_files() -> List[str]:
+    generated_files = get_generated_pml_files()
+
+    trails = glob.glob(f"*.trail")
+    generated_files += trails
+    generated_files += glob.glob(f"*.spn")
+    generated_files += glob.glob(f"tc-*.c")
+    if len(trails) == 1:
+        generated_files += glob.glob(f"tr-*-0.c")
+    else:
+        generated_files += glob.glob(f"tr-*-*.c")
+    generated_files += glob.glob(f"*-test.log")
+    return generated_files
+
+
+def get_programs(model_name):
     parser = yacc.Parser(ast, lex.Lexer())
-    parsed = parser.parse(None, model_file)
+    parsed = parser.parse(None, f"{model_name}.pml")
     return get_all_programs(parsed)
 
 
 def write_all_programs(all_programs: List[ProgramWithNegations]):
-    pathlib.Path("_").mkdir(exist_ok=True)
+    config = get_config(os.getcwd())
     name_to_num = dict()
     for program in all_programs:
-        if type(program.expression) == ast.LTL:
-            file_name = f"ltl_{program.name}.pml"
-            write_file(file_name, program.program)
-        elif type(program.expression) == ast.Assert:
-            if program.name not in name_to_num.keys():
-                name_to_num[program.name] = 0
-            else:
-                name_to_num[program.name] += 1
-            file_name = f"assert_{program.name}_{name_to_num[program.name]}.pml"
-            write_file(file_name, program.program)
+        if program.name not in config["disable_negation_at"]:
+            if type(program.expression) == ast.LTL:
+                file_name = f"ltl_{program.name}.pml"
+                write_file(file_name, program.program)
+            elif type(program.expression) == ast.Assert:
+                if program.name not in name_to_num.keys():
+                    name_to_num[program.name] = 0
+                else:
+                    name_to_num[program.name] += 1
+                file_name = f"assert_{program.name}_{name_to_num[program.name]}.pml"
+                write_file(file_name, program.program)
 
 
-def main(args):
-    model_file = args[1]
-    programs = get_programs(model_file)
+def generate_pml_files(model_name):
+    programs = get_programs(model_name)
     write_all_programs(programs)
 
 
+def generate_spin_files(model_name, config, source_dir):
+    pml_files = get_generated_pml_files()
+    for pml_file in pml_files:
+        if pml_file.startswith("assert"):
+            spinallscenarios = config["spin_assert"]
+        elif pml_file.startswith("ltl"):
+            ltl_name = pml_file.lstrip("ltl_").rstrip(".pml")
+            spinallscenarios = f"{config['spin_assert']}{ltl_name}"
+        refine_config = testbuilder.get_refine_config(source_dir, model_name,
+                                                      os.getcwd())
+        pml_name = pml_file.rstrip(".pml")
+        testbuilder.generate_spin_files(pml_name, os.getcwd(),
+                                        spinallscenarios, refine_config)
+
+
+def generate_test_files(model_name, source_dir):
+    pml_files = get_generated_pml_files()
+    for pml_file in pml_files:
+        refine_config = testbuilder.get_refine_config(source_dir, model_name,
+                                                      os.getcwd())
+        testbuilder_config = testbuilder.get_config(source_dir)
+        pml_name = pml_file.rstrip(".pml")
+        testbuilder.generate_test_files(pml_name, os.getcwd(),
+                                        testbuilder.get_config(testbuilder_config),
+                                        refine_config)
+
+
+def clean(model_name, model_dir):
+    """Cleans out generated files in current directory"""
+    cwd = os.getcwd()
+    os.chdir(model_dir)
+    print(f"Removing spin and test files for {model_name}")
+    files = get_generated_files()
+    for file in files:
+        os.remove(file)
+    os.chdir(cwd)
+
+
+def main(args):
+    source_dir = os.path.dirname(os.path.realpath(__file__))
+    config = get_config(source_dir)
+    command = args[1]
+    if len(args) > 2:
+        model_name = args[2]
+    if command == "genpmls":
+        generate_pml_files(model_name)
+    elif command == "spin":
+        generate_spin_files(model_name, config, source_dir)
+    elif command == "gentests":
+        generate_test_files(model_name, source_dir)
+    elif command == "clean":
+        clean(model_name, os.getcwd())
+    elif command == "compile":
+        testbuilder_config = testbuilder.get_config(source_dir)
+        testbuilder.compile(testbuilder_config["rtems"])
+    elif command == "run":
+        testbuilder_config = testbuilder.get_config(source_dir)
+        testbuilder.run_simulator(testbuilder_config["simulator"],
+                                  testbuilder_config["simulatorargs"],
+                                  testbuilder_config["testexe"],
+                                  testbuilder_config["testsuite"])
+
+
 if __name__ == "__main__":
-    print(sys.argv)
     main(sys.argv)
 
